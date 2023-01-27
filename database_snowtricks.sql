c20bcaa53a0a347a84bf6ffe85388ee94e97abdd
@ludoviclemaitre <contact@llemaitre.com>
2023-01-27 23:00:35 +0100

    Update database + initial dataset

--- database_snowtricks.sql
+++ database_snowtricks.sql
@@ -5,9 +5,9 @@
 # https://sequel-ace.com/
 # https://github.com/Sequel-Ace/Sequel-Ace
 #
-# Host: 127.0.0.1 (MySQL 8.0.31)
+# Host: 127.0.0.1 (MySQL 8.0.32)
 # Database: snowtricks
-# Generation Time: 2023-01-15 11:38:30 +0000
+# Generation Time: 2023-01-27 21:31:41 +0000
 # ************************************************************
 
 
@@ -40,8 +40,8 @@ LOCK TABLES `category` WRITE;
 
 INSERT INTO `category` (`id_category`, `title`, `slug`, `date_add`)
 VALUES
-	(1,'grab','grab','2022-10-16 16:37:53'),
-	(2,'other','other','2022-10-23 11:30:52');
+	(1,'Grabs','grabs','2023-01-27 21:35:32'),
+	(2,'Rotations','rotations','2023-01-27 21:35:32');
 
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -73,7 +73,9 @@ VALUES
 	('DoctrineMigrations\\Version20221211162849','2022-12-11 16:29:29',50),
 	('DoctrineMigrations\\Version20221231145514','2022-12-31 18:27:29',48),
 	('DoctrineMigrations\\Version20230113144154','2023-01-15 10:52:06',45),
-	('DoctrineMigrations\\Version20230115105302','2023-01-15 11:37:52',52);
+	('DoctrineMigrations\\Version20230115105302','2023-01-15 11:37:52',52),
+	('DoctrineMigrations\\Version20230115204308','2023-01-15 20:44:34',91),
+	('DoctrineMigrations\\Version20230122140425','2023-01-22 14:04:56',57);
 
 /*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -91,7 +93,7 @@ CREATE TABLE `image` (
   PRIMARY KEY (`id_image`),
   UNIQUE KEY `UNIQ_C53D045FF47645AE` (`url`),
   KEY `IDX_C53D045FB281BE2E` (`trick_id`),
-  CONSTRAINT `FK_C53D045FB281BE2E` FOREIGN KEY (`trick_id`) REFERENCES `trick` (`id_trick`)
+  CONSTRAINT `FK_C53D045FB281BE2E` FOREIGN KEY (`trick_id`) REFERENCES `trick` (`id_trick`) ON DELETE SET NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 LOCK TABLES `image` WRITE;
@@ -99,22 +101,12 @@ LOCK TABLES `image` WRITE;
 
 INSERT INTO `image` (`id_image`, `trick_id`, `url`)
 VALUES
-	(1,1,'/img/Picswiss-VD-44-23-6361257049384.jpg'),
-	(2,2,'/img/snowboard-neige-figure-saut-shutterstock-3516624621-63612cd79e80c.jpg'),
-	(3,3,'/img/snowboard-neige-figure-saut-shutterstock-636530a905259.jpg'),
-	(4,4,'/img/Picswiss-VD-44-23-636539f048adc.jpg'),
-	(5,4,'/img/snowboard-neige-figure-saut-shutterstock-636539f048eec.jpg'),
-	(6,5,'/img/Picswiss-VD-44-23-6365440404c6f.jpg'),
-	(7,5,'/img/snowboard-neige-figure-saut-shutterstock-6365440404fd8.jpg'),
-	(8,4,'/img/Picswiss-VD-44-23-636544730bb5e.jpg'),
-	(10,7,'/img/Picswiss-VD-44-23-636581b36312d.jpg'),
-	(11,8,'/img/28-winters-nitro-knut-eliassen-carving-636fcd9456243.avif'),
-	(12,8,'/img/AgAAAB0Avp62vmokNkONBFidgXn3JQ-636fcd945660f.avif'),
-	(13,4,'/img/JessPressWeb-1400x-636fd71b9355a.webp'),
-	(14,4,'/img/KellyClark-TrickTips-Blotto-9-2e16d0ba-fill-1000x800-c75-636fd7b3c6812.jpg'),
-	(15,4,'/img/merlin-186346947-e4e67564-bede-49ac-97fb-c7df58c0b62c-videoSixteenByNine3000-636fd7b3c6c95.jpg'),
-	(16,4,'/img/28-winters-nitro-knut-eliassen-carving-6370d54dbd916.avif'),
-	(17,4,'/img/AgAAAB0Avp62vmokNkONBFidgXn3JQ-6370d54dbe187.avif');
+	(1,1,'/img/Tous-les-mots-quil-faut-connaitre-lorsque-lon-aime-le-surf-71-63d4408437ada.jpg'),
+	(2,1,'/img/AgAAAB0Avp62vmokNkONBFidgXn3JQ-63d44084380be.avif'),
+	(3,1,'/img/KellyClark-TrickTips-Blotto-9-2e16d0ba-fill-1000x800-c75-63d4408438203.jpg'),
+	(4,1,'/img/mute-air-on-snowboard-1438217-63d44084382d9.jpg'),
+	(5,1,'/img/snowboard-lexique-63d44084383a3.avif'),
+	(6,1,'/img/c0b199d73b-120515-lexique-snowboard-63d440843849f.jpg');
 
 /*!40000 ALTER TABLE `image` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -129,15 +121,12 @@ CREATE TABLE `message` (
   `id_message` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
   `trick_id` int NOT NULL,
-  `status_id` int NOT NULL,
   `contents` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `date_add` datetime NOT NULL,
   `date_updated` datetime NOT NULL,
   PRIMARY KEY (`id_message`),
   KEY `IDX_B6BD307FA76ED395` (`user_id`),
   KEY `IDX_B6BD307FB281BE2E` (`trick_id`),
-  KEY `IDX_B6BD307F6BF700BD` (`status_id`),
-  CONSTRAINT `FK_B6BD307F6BF700BD` FOREIGN KEY (`status_id`) REFERENCES `status` (`id_status`),
   CONSTRAINT `FK_B6BD307FA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`),
   CONSTRAINT `FK_B6BD307FB281BE2E` FOREIGN KEY (`trick_id`) REFERENCES `trick` (`id_trick`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -145,15 +134,19 @@ CREATE TABLE `message` (
 LOCK TABLES `message` WRITE;
 /*!40000 ALTER TABLE `message` DISABLE KEYS */;
 
-INSERT INTO `message` (`id_message`, `user_id`, `trick_id`, `status_id`, `contents`, `date_add`, `date_updated`)
+INSERT INTO `message` (`id_message`, `user_id`, `trick_id`, `contents`, `date_add`, `date_updated`)
 VALUES
-	(1,1,4,2,'Test mentorat commentaire.','2022-12-23 12:15:03','2022-12-23 12:15:03'),
-	(2,1,4,1,'Test mentorat commentaire.','2022-12-23 12:18:46','2022-12-23 12:18:46'),
-	(3,1,4,2,'Test mentorat commentaire 2.','2022-12-23 12:23:30','2022-12-23 12:23:30'),
-	(4,1,4,2,'Test mentorat commentaire 3.','2022-12-23 12:31:42','2022-12-23 12:31:42'),
-	(5,1,4,2,'Test mentorat commentaire 4.','2022-12-23 12:44:05','2022-12-23 12:44:05'),
-	(6,1,4,2,'Test mentorat commentaire 5.','2022-12-23 12:45:49','2022-12-23 12:45:49'),
-	(7,1,4,2,'Test mentorat commentaire 6.','2022-12-23 12:50:25','2022-12-23 12:50:25');
+	(1,1,1,'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis posuere venenatis vehicula. Praesent imperdiet augue a massa tincidunt, ac vehicula tortor luctus.','2023-01-27 22:25:22','2023-01-27 22:25:22'),
+	(2,1,1,'Suspendisse hendrerit nisi at massa posuere rhoncus. Vivamus maximus volutpat nibh in sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.','2023-01-27 22:25:39','2023-01-27 22:25:39'),
+	(3,1,1,'Nunc lacinia metus sit amet hendrerit porttitor.','2023-01-27 22:26:07','2023-01-27 22:26:07'),
+	(4,1,1,'Vivamus non nisi magna.','2023-01-27 22:26:39','2023-01-27 22:26:39'),
+	(5,1,1,'Aenean risus nulla, molestie sed scelerisque et, finibus non sapien.','2023-01-27 22:26:56','2023-01-27 22:26:56'),
+	(6,1,1,'Etiam in mattis massa. Maecenas at magna mauris. Morbi eu orci molestie, malesuada ante ac, aliquet ligula. Nulla iaculis lacinia mi, quis scelerisque quam ultricies eu.','2023-01-27 22:27:26','2023-01-27 22:27:26'),
+	(7,1,1,'Nulla at lacus sollicitudin, varius eros et, ultrices sapien. Praesent accumsan mattis finibus.','2023-01-27 22:27:40','2023-01-27 22:27:40'),
+	(8,1,1,'Mauris mollis sagittis purus. Pellentesque et condimentum lorem. Phasellus vitae turpis euismod elit hendrerit tempus ac id enim.','2023-01-27 22:28:12','2023-01-27 22:28:12'),
+	(9,1,1,'Mauris accumsan interdum justo. Suspendisse malesuada lacus ac erat fermentum, id blandit magna lobortis. Maecenas lectus elit, ullamcorper a massa accumsan, malesuada dapibus libero.','2023-01-27 22:28:30','2023-01-27 22:28:30'),
+	(10,1,1,'Curabitur eget neque feugiat, commodo dolor ac, tempus augue. Praesent eget lacus metus. Nulla eu porta leo, in blandit justo.','2023-01-27 22:28:44','2023-01-27 22:28:44'),
+	(11,1,1,'Quisque condimentum tincidunt nulla sed finibus. Nam sollicitudin velit nec magna congue, vel commodo tortor tempor. In eu massa volutpat, laoreet nisl nec, gravida lacus.','2023-01-27 22:29:09','2023-01-27 22:29:09');
 
 /*!40000 ALTER TABLE `message` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -180,30 +173,6 @@ CREATE TABLE `messenger_messages` (
 
 
 
-# Dump of table status
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `status`;
-
-CREATE TABLE `status` (
-  `id_status` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`id_status`),
-  UNIQUE KEY `UNIQ_7B00651C5E237E06` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-LOCK TABLES `status` WRITE;
-/*!40000 ALTER TABLE `status` DISABLE KEYS */;
-
-INSERT INTO `status` (`id_status`, `name`)
-VALUES
-	(1,'En attente'),
-	(2,'Publié');
-
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
 # Dump of table trick
 # ------------------------------------------------------------
 
@@ -218,7 +187,7 @@ CREATE TABLE `trick` (
   `contents` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `slug` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `published` smallint NOT NULL,
-  `deleted` smallint NOT NULL,
+  `deleted` smallint NOT NULL DEFAULT '0',
   `date_add` datetime NOT NULL,
   `date_updated` datetime NOT NULL,
   PRIMARY KEY (`id_trick`),
@@ -228,7 +197,7 @@ CREATE TABLE `trick` (
   KEY `IDX_D8F0A91EA76ED395` (`user_id`),
   KEY `IDX_D8F0A91E3DA5256D` (`image_id`),
   CONSTRAINT `FK_D8F0A91E12469DE2` FOREIGN KEY (`category_id`) REFERENCES `category` (`id_category`),
-  CONSTRAINT `FK_D8F0A91E3DA5256D` FOREIGN KEY (`image_id`) REFERENCES `image` (`id_image`),
+  CONSTRAINT `FK_D8F0A91E3DA5256D` FOREIGN KEY (`image_id`) REFERENCES `image` (`id_image`) ON DELETE SET NULL,
   CONSTRAINT `FK_D8F0A91EA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -237,16 +206,16 @@ LOCK TABLES `trick` WRITE;
 
 INSERT INTO `trick` (`id_trick`, `category_id`, `user_id`, `image_id`, `title`, `contents`, `slug`, `published`, `deleted`, `date_add`, `date_updated`)
 VALUES
-	(1,1,1,1,'mute','Test mute.','mute',1,0,'2022-11-01 13:56:00','2022-11-01 13:56:00'),
-	(2,1,1,2,'sad','Test sad.','sad',1,0,'2022-11-01 14:27:35','2022-11-01 14:27:35'),
-	(3,1,1,3,'test','Test.','test',1,1,'2022-11-04 15:42:04','2022-11-04 15:42:04'),
-	(4,1,1,17,'Test mentorat','Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in elementum lacus. Nam luctus consequat suscipit. Integer interdum justo non elit eleifend maximus ut in urna. Sed erat felis, suscipit ac convallis in, euismod at purus. Praesent lacus velit, condimentum sollicitudin viverra at, commodo nec dolor. Suspendisse facilisis fermentum nibh, eu rhoncus nisi porttitor sit amet. Maecenas eu maximus magna. Etiam vitae auctor sapien, nec condimentum ex. Donec gravida metus ac erat pellentesque, ac elementum turpis sodales. Donec vehicula porta maximus.\r\n\r\nDonec in enim eget purus mattis luctus eget eu ante. Aliquam erat volutpat. Proin malesuada nisi non orci volutpat, eu venenatis ex bibendum. Morbi iaculis lorem vitae dignissim aliquam. Proin luctus purus nisl, ac faucibus orci ultricies eu. Nulla facilisi. Sed ut dolor a mauris posuere commodo. Quisque rhoncus feugiat mi et posuere. Curabitur condimentum non neque nec pretium. Nulla scelerisque aliquam libero, nec bibendum felis hendrerit eget. Nam viverra vel elit in ornare. Nulla ornare justo faucibus, mattis purus non, efficitur metus. Praesent faucibus vel nulla vitae eleifend. Vestibulum in pretium erat, non pulvinar ante. Sed nec augue est. Pellentesque venenatis congue tellus quis vulputate.\r\n\r\nCras molestie vulputate leo, nec ultricies nunc ornare eget. Nunc tempor mattis hendrerit. Morbi cursus ut ipsum eu dignissim. Quisque pulvinar massa vitae semper scelerisque. In dictum leo neque, non blandit dui luctus nec. Donec pellentesque aliquam libero, vel dictum neque faucibus sit amet. Aenean volutpat orci neque, id tempus odio posuere at.','testmentorat',1,0,'2022-11-04 16:57:23','2022-11-27 18:54:02'),
-	(5,1,1,7,'test3','Test 3.','test3',1,0,'2022-11-04 16:55:32','2022-11-04 16:55:32'),
-	(7,1,1,10,'test4','Test 4.','test4',1,0,'2022-11-04 21:19:45','2022-11-04 21:19:45'),
-	(8,2,1,12,'Test5','Test 5.','test5',1,0,'2022-11-12 16:45:08','2022-11-12 16:45:08'),
-	(9,1,1,NULL,'Test6','Test 6.','test6',1,0,'2022-12-03 15:40:49','2022-12-03 15:40:49'),
-	(10,1,1,NULL,'Test7','Nam quis fringilla lectus. Donec sit amet risus eu arcu tincidunt pharetra. Vestibulum fringilla congue erat ac mollis. Mauris commodo laoreet sollicitudin. Etiam fermentum eros vitae tempor blandit. Curabitur blandit ligula molestie pellentesque dignissim. Aliquam et quam ut turpis cursus suscipit blandit nec nibh. Ut lacinia dapibus nibh, sit amet vestibulum odio bibendum convallis. Vestibulum rhoncus vel neque nec ornare. Ut ut odio lectus. Curabitur in vulputate mauris. Vestibulum placerat, nisl at consequat congue, nisi leo consequat nulla, in dignissim erat ipsum non arcu. Nulla fringilla tellus est, id facilisis orci tempor sit amet.','test7',1,0,'2022-12-10 10:46:46','2022-12-10 10:46:46'),
-	(11,1,1,NULL,'Test soutenance','Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Duis elementum tellus eget libero maximus, eu vulputate ex elementum. Suspendisse id placerat urna. Sed eu risus auctor, ultrices nibh sed, varius tellus. Nullam ac est mollis libero commodo aliquet ac vel dolor. Sed sed turpis urna. Maecenas nisl nunc, congue quis porta et, rutrum eget magna. Proin accumsan eleifend odio, sit amet aliquam diam aliquet ac. In id odio eu nisl feugiat semper et non mauris. Sed malesuada libero at urna tristique gravida. Vestibulum viverra imperdiet urna, quis faucibus ipsum tincidunt in. Aliquam feugiat pellentesque convallis. Curabitur pretium tempor condimentum. In vel dignissim ligula.','testsoutenance',1,0,'2023-01-11 08:16:17','2023-01-11 09:03:09');
+	(1,1,1,6,'mute','Saisie de la carre frontside de la planche entre les deux pieds avec la main avant.\r\n\r\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, urna sed pharetra sollicitudin, nunc eros aliquet augue, et mollis ante lacus vitae felis. Fusce finibus posuere rutrum. Nullam sed dictum mi. Vivamus dignissim leo odio, ac sodales felis posuere rhoncus. In malesuada felis sed mauris blandit porttitor. Donec bibendum at augue ac dictum. Etiam eget sagittis eros. Integer fringilla eros dolor, porta pharetra arcu finibus at. Curabitur ornare fermentum nunc, eu efficitur orci aliquet eu.\r\n\r\nMorbi fringilla ante magna, non eleifend magna ornare a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas malesuada rutrum enim, et ultrices diam feugiat eu. Vivamus et eleifend risus. Sed sit amet arcu eget leo porttitor fermentum. Mauris semper gravida ligula, non mollis ipsum malesuada vitae. Fusce consectetur vitae nunc id scelerisque. Curabitur iaculis suscipit diam, sed laoreet odio sollicitudin et. Morbi metus velit, egestas sit amet lectus nec, cursus dignissim lacus. \r\n\r\n Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam malesuada, urna sed pharetra sollicitudin, nunc eros aliquet augue, et mollis ante lacus vitae felis. Fusce finibus posuere rutrum. Nullam sed dictum mi. Vivamus dignissim leo odio, ac sodales felis posuere rhoncus. In malesuada felis sed mauris blandit porttitor. Donec bibendum at augue ac dictum. Etiam eget sagittis eros. Integer fringilla eros dolor, porta pharetra arcu finibus at. Curabitur ornare fermentum nunc, eu efficitur orci aliquet eu.\r\n\r\nMorbi fringilla ante magna, non eleifend magna ornare a. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas malesuada rutrum enim, et ultrices diam feugiat eu. Vivamus et eleifend risus. Sed sit amet arcu eget leo porttitor fermentum. Mauris semper gravida ligula, non mollis ipsum malesuada vitae. Fusce consectetur vitae nunc id scelerisque. Curabitur iaculis suscipit diam, sed laoreet odio sollicitudin et. Morbi metus velit, egestas sit amet lectus nec, cursus dignissim lacus.','mute',1,0,'2023-01-27 21:35:32','2023-01-27 22:16:56'),
+	(2,1,1,NULL,'sad','Saisie de la carre backside de la planche, entre les deux pieds, avec la main avant.','sad',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(3,1,1,NULL,'indy','Saisie de la carre frontside de la planche, entre les deux pieds, avec la main arrière.','indy',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(4,1,1,NULL,'stalefish','Saisie de la carre backside de la planche entre les deux pieds avec la main arrière.','stalefish',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(5,1,1,NULL,'tail grab','Saisie de la partie arrière de la planche, avec la main arrière.','tail%20grab',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(6,1,1,NULL,'nose grab','Saisie de la partie avant de la planche, avec la main avant.','nose%20grab',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(7,1,1,NULL,'japan','Saisie de l\'avant de la planche, avec la main avant, du côté de la carre frontside.','japan',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(8,1,1,NULL,'seat belt','Saisie du carre frontside à l\'arrière avec la main avant.','seat%20belt',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(9,1,1,NULL,'truck driver','Saisie du carre avant et carre arrière avec chaque main (comme tenir un volant de voiture).','truck%20driver',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32'),
+	(10,2,1,NULL,'180','Un 180 désigne un demi-tour, soit 180 degrés d\'angle.','180',1,0,'2023-01-27 21:35:32','2023-01-27 21:35:32');
 
 /*!40000 ALTER TABLE `trick` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -265,7 +234,7 @@ CREATE TABLE `user` (
   `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `profile_picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `deleted` smallint NOT NULL,
+  `deleted` smallint NOT NULL DEFAULT '0',
   `registration_date` datetime NOT NULL,
   `unsubscribe_date` datetime DEFAULT NULL,
   `is_verified` tinyint(1) NOT NULL,
@@ -281,8 +250,7 @@ LOCK TABLES `user` WRITE;
 
 INSERT INTO `user` (`id_user`, `username`, `email`, `password`, `lastname`, `firstname`, `profile_picture`, `deleted`, `registration_date`, `unsubscribe_date`, `is_verified`, `roles`, `reset_token`)
 VALUES
-	(1,'Nerofaust','ludoviclemaitre@orange.fr','$2y$13$itkkltUjK3e75HxEl/9BYuTB3Rl9vN9807OqStIHEO9He8sIMv76i','Lemaître','Ludovic','/img/ludovic-63bafa7be0bb7.png',0,'2022-10-16 16:17:12',NULL,1,'[\"ROLE_USER\"]',NULL),
-	(2,'Test','contact@llemaitre.com','$2y$13$7kN5yAS34/mQoSytzhGVKeyO25HjxfRpyQJUJwxp4ect0VdwfOP/a',NULL,NULL,NULL,0,'2022-12-22 13:04:04',NULL,0,'[\"ROLE_USER\"]',NULL);
+	(1,'Nerofaust','contact@llemaitre.com','$2y$13$VNspw1VgW.bNTLJJdSG7RuYwVHIecsp2nCK9YH.iW6F8kAsQnMNiO','Lemaître','Ludovic','/img/ludovic-63d43cc810385.png',0,'2023-01-27 21:35:32',NULL,1,'[\"ROLE_USER\"]',NULL);
 
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -308,14 +276,10 @@ LOCK TABLES `video` WRITE;
 
 INSERT INTO `video` (`id_video`, `trick_id`, `url`)
 VALUES
-	(1,2,'https://www.youtube.com/embed/oLnvs5IdPFg'),
-	(9,4,'https://www.youtube.com/embed/M2gSx16vmno'),
-	(4,4,'https://www.youtube.com/embed/rHMDRN6E9CE'),
-	(2,4,'https://www.youtube.com/embed/XqTq53lQhKU'),
-	(3,4,'https://www.youtube.com/embed/zqNXN-CG0kI'),
-	(6,10,'https://www.dailymotion.com/embed/video/x8ci1yx'),
-	(5,10,'https://www.youtube.com/embed/Fxp6K-I4-VY'),
-	(7,10,'https://www.youtube.com/embed/ULY6RQ50Eg4');
+	(3,1,'https://www.youtube.com/embed/CA5bURVJ5zk'),
+	(2,1,'https://www.youtube.com/embed/M5NTCfdObfs'),
+	(4,1,'https://www.youtube.com/embed/NnnsXEBwTHc'),
+	(1,1,'https://www.youtube.com/embed/wWgCIEpE0Ug');
 
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
